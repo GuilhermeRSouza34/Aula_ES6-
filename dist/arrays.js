4c4f6dff27b96a34169ecee7371cfaa67a4b2be5
Guilherme <guilhermersouza34@gmail.com>
2024-04-16 15:04:04 -0300

    Conhecendo o Metoto Map

---
+++ dist/arrays.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var redesSociais = ['Facebook', 'instagram', 'X'];
+for (var i = 0; i < redesSociais.length; i++) {
+  console.log("Eu tenho Perfil na rede Social: ".concat(redesSociais[i]));
+}
---
+++ src/arrays.js
@@ -0,0 +1,11 @@
+const redesSociais = ['Facebook', 'instagram', 'X'];
+
+for (let i = 0; i < redesSociais.length; i++) {
+    console.log(`Eu tenho Perfil na rede Social: ${redesSociais[i]}`);
+}
+
+redesSociais.forEach(function(NomeDaRedeSocial, indice) {
+    console.log(`#${indice} Eu tenho Perfil na rede Social: ${NomeDaRedeSocial}`);
+})
+
+const alunos = ['Gustavo', 'Roberto', 'Ana', 'Malu'];
