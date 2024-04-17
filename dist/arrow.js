88b8d6f327ceae8745a86cb54234b0d5d3a0b9b8
Guilherme <guilhermersouza34@gmail.com>
2024-04-17 12:00:40 -0300

    Aula Sobre as Arrow Functions

--- dist/arrow.js
+++ dist/arrow.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var _this = void 0;
 var minhaFuncao = function minhaFuncao() {
   return "Ola Mundo";
 };
@@ -11,3 +12,17 @@ var retornaUmCarro = function retornaUmCarro() {
 };
 console.log(minhaFuncao());
 console.log(retornaUmCarro());
+var carro = {
+  velocidadeAtual: 40,
+  acelerar: function acelerar() {
+    console.log(this);
+    this.velocidadeAtual += 10;
+  },
+  frear: function frear() {
+    console.log(_this);
+    _this.velocidadeAtual -= 10;
+  }
+};
+carro.acelerar();
+carro.frear();
+console.log(carro.velocidadeAtual);
--- src/arrow.js
+++ src/arrow.js
@@ -7,3 +7,20 @@ const retornaUmCarro = () => ({
      
 console.log(minhaFuncao());
 console.log(retornaUmCarro());
+
+const carro = {
+    velocidadeAtual: 40,
+    acelerar: function () {
+        console.log(this)
+        this.velocidadeAtual += 10;
+    },
+    frear: () => {
+        console.log(this)
+        this.velocidadeAtual -= 10;
+    }
+}
+
+carro.acelerar();
+carro.frear();
+
+console.log(carro.velocidadeAtual);
