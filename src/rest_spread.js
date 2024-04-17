f147db001234cac479560d07480d7cbfb7aaebdd
Guilherme <guilhermersouza34@gmail.com>
2024-04-17 12:12:19 -0300

    Aula Sobre rest e Spread - arguments

--- src/rest_spread.js
+++ src/rest_spread.js
@@ -1,13 +1,14 @@
-function somar(a, b) {
+function somar() {
     let soma = 0;
 
     for (let i = 0; i < arguments.length; i++) {
         soma += arguments[i];
     }
 
+    return soma;
     //arguments.map
     //console.log(arguments)
     //return a + b;
 }
 
-console.log(somar(10, 20))
+console.log(somar(10, 20, 30))
