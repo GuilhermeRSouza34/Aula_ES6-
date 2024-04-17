a63e77f2a201011ddcf077d1fcd984e377e1f5a9
Guilherme <guilhermersouza34@gmail.com>
2024-04-17 14:38:32 -0300

    Aula Sobre map e set

--- dist/map_set.js
+++ dist/map_set.js
@@ -1,11 +1,72 @@
 "use strict";
 
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
 var meuMap = new Map();
 meuMap.set("nome", 'Guilherme');
+meuMap.set("stack", "HTML, CSS, JavaScript");
 console.log(meuMap);
 var nome = meuMap.get("nome");
 console.log(nome);
 console.log(meuMap.size);
 console.log(meuMap.has("sobrenome"));
-meuMap.clear();
+
+//meuMap.clear();
 console.log(meuMap.size);
+var _iterator = _createForOfIteratorHelper(meuMap.keys()),
+  _step;
+try {
+  for (_iterator.s(); !(_step = _iterator.n()).done;) {
+    var chave = _step.value;
+    console.log(chave);
+  }
+} catch (err) {
+  _iterator.e(err);
+} finally {
+  _iterator.f();
+}
+var _iterator2 = _createForOfIteratorHelper(meuMap.values()),
+  _step2;
+try {
+  for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
+    var valor = _step2.value;
+    console.log(valor);
+  }
+} catch (err) {
+  _iterator2.e(err);
+} finally {
+  _iterator2.f();
+}
+var _iterator3 = _createForOfIteratorHelper(meuMap.entries()),
+  _step3;
+try {
+  for (_iterator3.s(); !(_step3 = _iterator3.n()).done;) {
+    var entrada = _step3.value;
+    console.log(entrada);
+  }
+} catch (err) {
+  _iterator3.e(err);
+} finally {
+  _iterator3.f();
+}
+meuMap.delete("stack");
+console.log(meuMap);
+
+//SET
+
+var cpfs = new Set();
+cpfs.add('11111111111');
+cpfs.add('22222222222');
+cpfs.add('33333333333');
+console.log(cpfs);
+console.log(cpfs.keys());
+console.log(cpfs.values());
+cpfs.forEach(function (valor) {
+  console.log(valor);
+});
+
+//Utilizando o Array como Set para nao colocar itens duplicados
+var array = ['Guilherme', 'Paulo', 'Ana', 'Ana', 'Paula', 'Paulo', 'Carlos', 'Bebeto'];
+var arrayComoSet = new Set([].concat(array));
+console.log(arrayComoSet);
--- src/map_set.js
+++ src/map_set.js
@@ -1,5 +1,6 @@
 let meuMap = new Map();
 meuMap.set("nome", 'Guilherme');
+meuMap.set("stack", "HTML, CSS, JavaScript");
 
 console.log(meuMap);
 
@@ -8,5 +9,41 @@ console.log(nome);
 console.log(meuMap.size)
 console.log(meuMap.has("sobrenome"))
 
-meuMap.clear();
+//meuMap.clear();
 console.log(meuMap.size);
+
+for (let chave of meuMap.keys()) {
+    console.log(chave);
+}
+
+for (let valor of meuMap.values()) {
+    console.log(valor);
+}
+
+for (let entrada of meuMap.entries()) {
+    console.log(entrada);
+}
+
+meuMap.delete("stack");
+console.log(meuMap);
+
+
+//SET
+
+const cpfs = new Set();
+cpfs.add('11111111111')
+cpfs.add('22222222222')
+cpfs.add('33333333333')
+
+console.log(cpfs);
+console.log(cpfs.keys());
+console.log(cpfs.values());
+
+cpfs.forEach((valor) => {
+    console.log(valor);
+})
+
+//Utilizando o Array como Set para nao colocar itens duplicados
+const array = ['Guilherme', 'Paulo', 'Ana', 'Ana', 'Paula', 'Paulo', 'Carlos', 'Bebeto']
+const arrayComoSet = new Set([...array]);
+console.log(arrayComoSet);
