f187c11eb0fcbf3684e32cd41116234a84ba5834
Guilherme <guilhermersouza34@gmail.com>
2024-04-17 15:23:20 -0300

    Aula Sobre Async

--- dist/async.js
+++ dist/async.js
@@ -7,6 +7,20 @@ function funcaoMuitoPesado() {
   }
   return execucoes;
 }
+var funcaoMuitoPesadoPromise = new Promise(function (resolve, reject) {
+  try {
+    var execucoes = 0;
+    for (var i = 0; i < 1000000000; i++) {
+      execucoes++;
+    }
+    resolve(execucoes);
+  } catch (e) {
+    reject('Deu erro');
+  }
+});
 console.log("inicio");
-funcaoMuitoPesado();
+//console.log(funcaoMuitoPesado());
+funcaoMuitoPesadoPromise.then(function (resultado) {
+  return console.log(resultado);
+});
 console.log("fim");
--- src/async.js
+++ src/async.js
@@ -6,6 +6,20 @@ function funcaoMuitoPesado() {
     return execucoes;
 }
 
-console.log("inicio")
-funcaoMuitoPesado();
+const funcaoMuitoPesadoPromise = new Promise((resolve, reject) => {
+    try {
+        let execucoes = 0;
+        for (let i = 0; i < 1000000000; i++ ) {
+            execucoes++;
+        }
+        resolve (execucoes);
+    } catch (e) {
+        reject ('Deu erro');
+    }
+})
+
+console.log("inicio");
+//console.log(funcaoMuitoPesado());
+funcaoMuitoPesadoPromise.then(resultado => console.log(resultado));
 console.log("fim");
+
